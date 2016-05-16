ac91ab8f0e4336019fabe5e050b3c56ffff534d5
James Lee <james@james.tl>
2016-05-16 12:10:27 -0400

    cursors: Make build more generic/build more sizes
    Breeze ships with 24 and 48 size cursors.  For common WQHD laptops,
    something in between, like 40, is preferable.  Given the cursor source
    is SVG, it would be possible to create arbitrary size cursors, but the
    build script and xcursorgen config files hardcoded the 24 and 48 sizes.
    
    This change turns the xcursorgen config files into something that can
    be processed to generate arbitrary size cursors.  The build script has
    been modified to iterate over a list of sizes that can be changed as
    desired without having to change any hardcoded values.  Pointer x-y
    hit values are interpolated, and where they fall between pixels, they
    are rounded to the nearest pixel.  This does not seem to affect
    usability, especially on the high-dpi displays for which the cursors
    are intended.
    
    This change also modifies the build script to support building multiple
    similar cursors, and merges in the Breeze_Snow cursor, eliminating the
    duplicate build systems between the two versions of the Breeze cursors.
    
    Lastly, we fix a broken alias for the n-resize cursor, from
    size-size_ver to size_ver.

--- cursors/Breeze/build.sh
+++ cursors/Breeze/build.sh
@@ -10,22 +10,11 @@
 #fi
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
-RAWSVG="src/cursors.svg"
-INDEX="src/index.theme"
 ALIASES="src/cursorList"
+SIZES="24 32 40 48 64 96"
 
 
-echo -ne "Checking Requirements...\\r"
-if [ ! -f $RAWSVG ] ; then
-	echo -e "\\nFAIL: '$RAWSVG' missing in /src"
-	exit 1
-fi
-
-if [ ! -f $INDEX ] ; then
-	echo -e "\\nFAIL: '$INDEX' missing in /src"
-	exit 1
-fi
-
+echo -ne "Checking requirements...\\r"
 if  ! type "inkscape" > /dev/null ; then
 	echo -e "\\nFAIL: inkscape must be installed"
 	exit 1
@@ -35,116 +24,141 @@ if  ! type "xcursorgen" > /dev/null ; then
 	echo -e "\\nFAIL: xcursorgen must be installed"
 	exit 1
 fi
-echo -e "Checking Requirements... DONE"
+echo -e "Checking requirements... DONE"
 
 
 
-echo -ne "Making Folders... $BASENAME\\r"
-DIR2X="build/x2"
-DIR1_5X="build/x1_5"
-DIR1X="build/x1"
-OUTPUT="$(grep --only-matching --perl-regex "(?<=Name\=).*$" $INDEX)"
-OUTPUT=${OUTPUT// /_}
-mkdir -p "$DIR2X"
-mkdir -p "$DIR1_5X"
-mkdir -p "$DIR1X"
-mkdir -p "$OUTPUT/cursors"
-echo 'Making Folders... DONE';
+preprocess_cursor_configs() {
+	local template basename i size xhot yhot filename delay newxhot newyhot
+
+	echo -ne "Preprocessing cursor configs...\\r"
+
+	[ ! -d "build/config" ] && mkdir -p "build/config"
+
+	for template in src/config/*.cursor.in; do
+		basename=${template##*/}
+		basename=${basename%%.*}
+		rm -f "build/config/$basename.cursor"
+		for i in $SIZES; do
+			while read size xhot yhot filename delay; do
+				newxhot=$(printf '%.0f' $(echo "$xhot * $i / $size" | bc -l))
+				newyhot=$(printf '%.0f' $(echo "$yhot * $i / $size" | bc -l))
+				echo "$i $newxhot $newyhot $i/$filename $delay" >> "build/config/$basename.cursor"
+			done < $template
+		done
+	done
+
+	echo -e "Preprocessing cursor configs... DONE"
+}
 
 
+generate_pixmaps() {
+	local basename=$1
+	local rawsvg=$2
+	local outdir=$3
+	local size
 
-for CUR in src/config/*.cursor; do
-	BASENAME=$CUR
-	BASENAME=${BASENAME##*/}
-	BASENAME=${BASENAME%.*}
+	for size in $SIZES; do
+		if [ "$outdir/$size/$basename.png" -ot $rawsvg ] ; then
+			inkscape -i $basename -d $(($size * 90 / 24)) -f $rawsvg -e "$outdir/$size/$basename.png" > /dev/null 2>&1
+			if [ $? -ne 0 ]; then
+				echo -e "\\nFAIL: inkscape failed to generate '$outdir/$size/$basename.png'"
+				exit 1
+			fi
+		fi
+	done
+}
 
-	echo -ne "\033[0KGenerating simple cursor pixmaps... $BASENAME\\r"
 
-	if [ "$DIR1X/$BASENAME.png" -ot $RAWSVG ] ; then
-		inkscape -i $BASENAME -d 90  -f $RAWSVG -e "$DIR1X/$BASENAME.png" > /dev/null
+generate_cursor() {
+	local name=$1
+	local rawsvg="src/$name/cursors.svg"
+	local index="src/$name/index.theme"
+	local output cursor basename i error from to
+
+	if [ ! -f $rawsvg ] ; then
+		echo -e "\\nFAIL: '$rawsvg' missing"
+		exit 1
 	fi
 
-	if [ "$DIR1_5X/$BASENAME.png" -ot $RAWSVG ] ; then
-		inkscape -i $BASENAME -d 135 -f $RAWSVG -e "$DIR1_5X/$BASENAME.png" > /dev/null
+	if [ ! -f $index ] ; then
+		echo -e "\\nFAIL: '$index' missing"
+		exit 1
 	fi
 
-	if [ "$DIR2X/$BASENAME.png" -ot $RAWSVG ] ; then
-		inkscape -i $BASENAME -d 180 -f $RAWSVG -e "$DIR2X/$BASENAME.png" > /dev/null
+	echo "Generating $name..."
+
+	echo -ne "Making folders...\\r"
+	output="$(grep --only-matching --perl-regex "(?<=Name\=).*$" $index)"
+	output=${output// /_}
+	mkdir -p "$output/cursors"
+	for size in $SIZES; do mkdir -p "build/$name/$size"; done
+	echo 'Making folders... DONE';
+
+
+	for cursor in build/config/*.cursor; do
+		basename=${cursor##*/}
+		basename=${basename%.*}
+
+		echo -ne "\033[0KGenerating simple cursor pixmaps... $basename\\r"
+		generate_pixmaps $basename $rawsvg "build/$name"
+	done
+	echo -e "\033[0KGenerating simple cursor pixmaps... DONE"
+
+
+
+	for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23; do
+		echo -ne "\033[0KGenerating animated cursor pixmaps... $i / 23 \\r"
+		generate_pixmaps "progress-$i" $rawsvg "build/$name"
+		generate_pixmaps "wait-$i" $rawsvg "build/$name"
+	done
+	echo -e "\033[0KGenerating animated cursor pixmaps... DONE"
+
+
+
+	echo -ne "Generating cursor theme...\\r"
+	for cursor in build/config/*.cursor; do
+		basename=${cursor##*/}
+		basename=${basename%.*}
+
+		error="$( xcursorgen -p "build/$name" "$cursor" "$output/cursors/$basename" 2>&1 )"
+
+		if [[ "$?" -ne "0" ]]; then
+			echo "FAIL: $cursor $error"
+		fi
+	done
+	echo -e "Generating cursor theme... DONE"
+
+
+
+	echo -ne "Generating shortcuts...\\r"
+	while read from to ; do
+		if [ -e "$output/cursors/$from" ] ; then
+			continue
+		fi
+
+		ln -s "$to" "$output/cursors/$from"
+	done < $ALIASES
+	echo -e "\033[0KGenerating shortcuts... DONE"
+
+
+
+	echo -ne "Copying theme index...\\r"
+		if ! [ -e "$output/$index" ] ; then
+			cp $index "$output/index.theme"
 	fi
-done
-echo -e "\033[0KGenerating simple cursor pixmaps... DONE"
+	echo -e "\033[0KCopying theme index... DONE"
+
+	echo -e "Generating $name... DONE"
+}
 
 
-
-for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23
-do
-	echo -ne "\033[0KGenerating animated cursor pixmaps... $i / 23 \\r"
-
-	if [ "$DIR1X/progress-$i.png" -ot $RAWSVG ] ; then
-		inkscape -i progress-$i -d 90  -f $RAWSVG -e "$DIR1X/progress-$i.png" > /dev/null
-	fi
-
-    if [ "$DIR1_5X/progress-$i.png" -ot $RAWSVG ] ; then
-        inkscape -i progress-$i -d 135 -f $RAWSVG -e "$DIR1_5X/progress-$i.png" > /dev/null
-	fi
-
-	if [ "$DIR2X/progress-$i.png" -ot $RAWSVG ] ; then
-		inkscape -i progress-$i -d 180 -f $RAWSVG -e "$DIR2X/progress-$i.png" > /dev/null
-	fi
-
-	if [ "$DIR1X/wait-$i.png" -ot $RAWSVG ] ; then
-		inkscape -i wait-$i -d 90  -f $RAWSVG -e "$DIR1X/wait-$i.png" > /dev/null
-	fi
-
-	if [ "$DIR1_5X/wait-$i.png" -ot $RAWSVG ] ; then
-		inkscape -i wait-$i -d 135  -f $RAWSVG -e "$DIR1_5X/wait-$i.png" > /dev/null
-	fi
-
-	if [ "$DIR2X/wait-$i.png" -ot $RAWSVG ] ; then
-		inkscape -i wait-$i -d 180 -f $RAWSVG -e "$DIR2X/wait-$i.png" > /dev/null
-	fi
-done
-echo -e "\033[0KGenerating animated cursor pixmaps... DONE"
-
-
-
-echo -ne "Generating cursor theme...\\r"
-for CUR in src/config/*.cursor; do
-	BASENAME=$CUR
-	BASENAME=${BASENAME##*/}
-	BASENAME=${BASENAME%.*}
-
-	ERR="$( xcursorgen -p build "$CUR" "$OUTPUT/cursors/$BASENAME" 2>&1 )"
-
-	if [[ "$?" -ne "0" ]]; then
-		echo "FAIL: $CUR $ERR"
-	fi
-done
-echo -e "Generating cursor theme... DONE"
-
-
-
-echo -ne "Generating shortcuts...\\r"
-while read ALIAS ; do
-	FROM=${ALIAS% *}
-	TO=${ALIAS#* }
-
-	if [ -e "$OUTPUT/cursors/$FROM" ] ; then
-		continue
-	fi
-
-	ln -s "$TO" "$OUTPUT/cursors/$FROM"
-done < $ALIASES
-echo -e "\033[0KGenerating shortcuts... DONE"
-
-
-
-echo -ne "Copying Theme Index...\\r"
-	if ! [ -e "$OUTPUT/$INDEX" ] ; then
-		cp $INDEX "$OUTPUT/index.theme"
-	fi
-echo -e "\033[0KCopying Theme Index... DONE"
-
+preprocess_cursor_configs
+echo
+generate_cursor Breeze
+echo
+generate_cursor Breeze_Snow
+echo
 
 
 echo "COMPLETE!"
